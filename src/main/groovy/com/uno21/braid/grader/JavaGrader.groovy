c2f8a6d51f7b25914efbe4160447172a1e6dc709
Lucas Videla <videla.lucas@gmail.com>
2013-04-03 11:29:05 -0300

    Se modifica el grader para admitir gradle y procesar los xml de resultado, devolviéndolos en la petición al socket

--- src/main/groovy/com/uno21/braid/grader/JavaGrader.groovy
+++ src/main/groovy/com/uno21/braid/grader/JavaGrader.groovy
@@ -5,6 +5,9 @@ import groovy.json.JsonOutput
 
 import org.apache.commons.io.FileUtils
 
+import com.uno21.braid.grader.parsers.java.checkstyle.CheckstyleParser
+import com.uno21.braid.grader.parsers.java.junit.JUnitParser
+
 class JavaGrader {
 	def completeMapWithQualifications(def map, def score, def detail) {
 		map.put('testScore', score)
@@ -14,40 +17,10 @@ class JavaGrader {
 	def jsonify(def map) {
 		new JsonOutput().toJson(new JsonBuilder().call(map))
 	}
-	
-	def processNotaTests(def myFile) {
-	
-		def fields = myFile.readLines().get(3).split(',').toList()
-		def totalTests = fields[0].split(':')[1].trim() as Integer
-		def fallos = fields[1].split(':')[1].trim() as Integer
-		def errores = fields[2].split(':')[1].trim() as Integer
-	
-		def testsCorrectos = totalTests - (fallos + errores)
-		// Calculamos la nota de tests
-		return 10 * (testsCorrectos/totalTests) as Double
-	}
-	
-	def processCheckstyle(def myFile) {
-		
-		def buffer = myFile.readLines()
-		def theXml = new XmlParser().parse(myFile)
-		
-		def incidencias = [] as List
-		theXml.file.each { file ->
-			file.error.each {
-			incidencias << "${file.attributes()['name'][file.attributes()['name'].lastIndexOf('src/')..-1]}:${it.attributes()['line']} -> ${it.attributes()['message']}"
-			}
-		}
-		
-		return incidencias
-	}
-	
+
 	def grade(def json) {
 	
-		def p = "git clone git@bitbucket.org:tallerweb/${json.nombreTarea}.git grader-${json.nombreTarea}".execute()
-		p.waitFor()
-		
-		p = "git checkout resolucion --git-dir grader-${json.nombreTarea}/.git".execute()
+		def p = "git clone -b resolucion git@bitbucket.org:tallerweb/${json.nombreTarea}.git grader-${json.nombreTarea}".execute()
 		p.waitFor()
 		
 		def rutaGraderConfiable = "./grader-${json.nombreTarea}"
@@ -56,7 +29,7 @@ class JavaGrader {
 		def nombreTemporalCarpeta = 'pull-' + new Date().time
 		def rutaTareaAlumno
 		
-		p = "git clone ${json.repoSshUrl} ${nombreTemporalCarpeta}".execute()
+		p = "git clone git@github.com:${json.githubUser}/${json.nombreTarea}.git ${nombreTemporalCarpeta}".execute()
 		p.waitFor()
 	
 		// No existía el repo, o hubo problemas con git
@@ -82,7 +55,7 @@ class JavaGrader {
 			return jsonify(correctionsMap)
 		}
 	
-		p = "mvn -f ${rutaTareaAlumno}/pom.xml compile".execute()
+		p = "gradle -p ${rutaTareaAlumno} compileJava".execute()
 		p.waitFor()
 	
 		// No compila el código
@@ -93,39 +66,46 @@ class JavaGrader {
 		}
 	
 		// Ejecutamos la tarea mvn test
-		p = "mvn -f ${rutaTareaAlumno}/pom.xml test".execute()
-		p.waitFor()
-		
-		p = "mvn -f ${rutaTareaAlumno}/pom.xml checkstyle:checkstyle".execute()
+		p = "gradle -p ${rutaTareaAlumno}".execute()
 		p.waitFor()
 	
 		// Tomamos el reporte y lo mostramos
 		try {
 	
-			def myTestsFile = new File("${rutaTareaAlumno}/target/surefire-reports/ar.edu.unlam.tallerweb.ecuaciones.EcuacionTest.txt")
-			def notaTests = processNotaTests(myTestsFile)
+			def junitParser = new JUnitParser()
+			def checkstyleParser = new CheckstyleParser()
 			
-			def incidenciasCheckstyle = processCheckstyle(new File("${rutaTareaAlumno}/target/checkstyle-result.xml"))
-			def textCheckstyle = incidenciasCheckstyle.join('\n')
-			def notaCheckstyle = (20 - incidenciasCheckstyle.size())/20 as Double
-			notaCheckstyle = notaCheckstyle > 0.0? notaCheckstyle : 0.0
+			def junitIssuesMap
 			
-			def nota = (notaTests * 8.0/10.0) + (notaCheckstyle * 2.0/10.0)
-	
+			def myTestsFile = new File("${rutaTareaAlumno}/build/test-results")
+			myTestsFile.eachFile {
+				junitIssuesMap = junitParser.parse(it)
+				// por el momento todos los tests se encuentran en el mismo archivo
+				// pero no sabemos el nombre exacto
+			}
+			def notaFuncional = junitIssuesMap["score"]
+			
+			
+			def myCheckstyleFile = new File("${rutaTareaAlumno}/build/reports/checkstyle/main.xml")
+			def checkstyleIssuesList = checkstyleParser.parse(myCheckstyleFile)
+			def notaEstilo = new Score((20 - checkstyleIssuesList.size())>0?((20 - checkstyleIssuesList.size())/10):0, 2)
+			
+			def nota = notaFuncional + notaEstilo
+			
 			def reporte = '=== Reporte completo ===\n'
-			reporte += "Nota general: ${nota.trunc(2)}/10.0\n"
-			reporte += "\tNota funcional: ${(notaTests*8.0)/10.0}/8.0\n"
-			reporte += "\tNota de estilo: ${(notaCheckstyle*2.0)/10.0}/2.0\n"
+			reporte += "Nota general: ${nota.normalize()}\n"
+			reporte += "\tNota funcional: ${notaFuncional}\n"
+			reporte += "\tNota de estilo: ${notaEstilo}\n"
 			reporte += '\n\n******************'
 			reporte += '\nAnálisis funcional\n'
 			reporte += '******************\n'
-			reporte += myTestsFile.text
+			reporte += reportarIssuesJUnit(junitIssuesMap)
 			reporte += '\n\n******************'
 			reporte += "\nAnálisis de estilo\n"
 			reporte += '******************\n'
-			reporte += textCheckstyle
+			reporte += reportarIssuesCheckstyle(checkstyleIssuesList)
 			
-			completeMapWithQualifications(correctionsMap, nota.trunc(2), reporte)
+			completeMapWithQualifications(correctionsMap, nota, reporte)
 	
 		} catch (FileNotFoundException e) {
 			e.printStackTrace()
@@ -143,5 +123,18 @@ class JavaGrader {
 	
 		return jsonify(correctionsMap)
 	
+	}
+
+	def reportarIssuesCheckstyle(def lista) {
+		lista ? lista.inject("") { acc, val -> acc +  val + "\n"} : 'Sin incidencias' 
+	}
+		
+	def reportarIssuesJUnit(def map) {
+		String result = ''
+		result += map['errors'].inject("") { acc, val -> acc +  val + "\n"}
+		result += '\n'
+		result += map['failures'].inject("") { acc, val -> acc +  val + "\n"}
+		
+		result
 	}
 }
